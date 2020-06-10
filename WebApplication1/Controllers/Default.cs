681ad7caad6b31d22424a163ea78b8bf39d30b91
Samu Lang <langsamu@hotmail.com>
2020-06-10 16:43:05 +0100

    Create web app proof-of-concept

---
+++ WebApplication1/Controllers/Default.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Controllers
+{
+    using System.Linq;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("")]
+    public class Default : Controller
+    {
+        [HttpGet("{thisMany}")]
+        public IActionResult Index(int thisMany) =>
+            this.View(thisMany.Primes().ToArray());
+    }
+}
---
+++ WebApplication1/Startup.cs
@@ -0,0 +1,40 @@
+namespace WebApplication1
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
